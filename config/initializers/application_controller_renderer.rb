be24aeab508eeeb6352018231c81fd982ca05a88
西雄太 <nishiyuuta@nishiyuutanoMacBook-Pro.local>
2019-06-10 18:21:12 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cf50994df4739dc6307062b3d9b808918dce9b7a2bc8bab39f1486a24d692a0756f715bc90dc6d19d2d570d7f1630e6d94db3391ef9eaa88aec8034874109ae8'
